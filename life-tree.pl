6bdc8bb7232b31213da014b77da8a5e9aa267f99
Vinicius Ferreira <vfbdelima@gmail.com>
2018-05-02 22:42:07 -0300

    Add data base of birds
    Co-authored-by: mendesiasmin <mendesiasmin96@gmail.com>
    Co-authored-by: joaorobson <joaorobsonmartins@gmail.com>

---
+++ life-tree.pl
@@ -0,0 +1,226 @@
+ancestral(neognathae, aves).
+ancestral(palaeognathae, aves).
+
+
+/*
+    descendentes de palaeognathae
+*/
+ancestral(struthioniformes, palaeognathae).
+ancestral(notopalaeognathae, palaeognathae).
+
+
+/*
+    descendentes de notopalaeognathae
+*/
+ancestral(rheiformes, notopalaeognathae).
+ancestral(novaeratitae, notopalaeognathae).
+
+
+/*
+    descendentes de novaeratitae
+*/
+ancestral(casuariiformes, novaeratitae).
+ancestral(tinamiformes, novaeratitae).
+ancestral(dinornithiformes, novaeratitae).
+ancestral(apterygiformes, novaeratitae).
+ancestral(aepyornithiformes, novaeratitae).
+
+
+/*
+    descendentes de neognathae
+*/
+ancestral(galloanserae, neognathae).
+ancestral(neoaves, neognathae).
+
+
+/*
+    descendentes de galloanserae
+*/
+ancestral(galliformes, galloanserae).
+ancestral(anseriformes, galloanserae).
+
+
+/*
+    descendentes de neoaves
+*/
+ancestral(strisores, neoaves).
+ancestral(columbaves, neoaves).
+ancestral(gruiformes, neoaves).
+ancestral(aequorlitornithes, neoaves).
+ancestral(inopinaves, neoaves).
+
+
+/*
+    descendentes de strisores
+*/
+ancestral(caprimulgiformes, strisores).
+ancestral(nyctibiiformes, strisores).
+ancestral(podargiformes, strisores).
+ancestral(apodiformes, strisores).
+
+
+/*
+    descendentes de caprimulgiformes
+*/
+ancestral(caprimulgidae, caprimulgiformes).
+
+
+/*
+    descendentes de nyctibiiformes
+*/
+ancestral(steatornithidae, nyctibiiformes).
+ancestral(nyctibiidae, nyctibiiformes).
+
+
+/*
+    descendentes de podargiformes
+*/
+ancestral(podargidae, podargiformes).
+
+
+/*
+    descendentes de columbaves
+*/
+ancestral(otidimorphae, columbaves).
+ancestral(columbimorphae, columbaves).
+
+
+/*
+    descendentes de otidimorphae
+*/
+ancestral(musophagiformes, otidimorphae).
+ancestral(otidiformes, otidimorphae).
+ancestral(cuculiformes, otidimorphae).
+
+
+/*
+    descendentes de columbimorphae
+*/
+ancestral(columbiformes, columbimorphae).
+ancestral(mesitornithiformes, columbimorphae).
+ancestral(pteroclidiformes, columbimorphae).
+
+
+/*
+    descendentes de aequorlitornithes
+*/
+ancestral(ardeae, aequorlitornithes).
+ancestral(mirandornithes, aequorlitornithes).
+ancestral(charadriiformes, aequorlitornithes).
+
+
+/*
+    descendentes de mirandornithes
+*/
+ancestral(phoenicopteriformes, mirandornithes).
+ancestral(podicipediformes, mirandornithes).
+
+
+/*
+    descendentes de ardeae
+*/
+ancestral(eurypygimorphae, ardeae).
+ancestral(aequornithes, ardeae).
+
+
+/*
+    descendentes de eurypygimorphae
+*/
+ancestral(phaethontiformes, eurypygimorphae).
+ancestral(eurypygiformes, eurypygimorphae).
+
+
+/*
+    descendentes de aequornithes
+*/
+ancestral(gaviiformes, aequornithes).
+ancestral(austrodyptornithes, aequornithes).
+ancestral(ciconiiformes, aequornithes).
+ancestral(suliformes, aequornithes).
+ancestral(pelecaniformes, aequornithes).
+
+
+/*
+    descendentes de austrodyptornithes
+*/
+ancestral(procellariiformes, austrodyptornithes).
+ancestral(sphenisciformes, austrodyptornithes).
+
+
+/*
+    descendentes de inopinaves
+*/
+ancestral(opisthocomiformes, inopinaves).
+ancestral(telluraves, inopinaves).
+
+
+/*
+    descendentes de telluraves
+*/
+ancestral(afroaves, telluraves).
+ancestral(australaves, telluraves).
+
+
+/*
+    descendentes de afroaves
+*/
+ancestral(accipitrimorphae, afroaves).
+ancestral(strigiformes, afroaves).
+ancestral(coraciimorphae, afroaves).
+
+
+/*
+    descendentes de accipitrimorphae
+*/
+ancestral(cathartiformes, accipitrimorphae).
+ancestral(accipitriformes, accipitrimorphae).
+
+
+/*
+    descendentes de coraciimorphae
+*/
+ancestral(coliidae, coraciimorphae).
+ancestral(cavitaves, coraciimorphae).
+
+
+/*
+    descendentes de cavitaves
+*/
+ancestral(leptosomatiformes, cavitaves).
+ancestral(eucavitaves, cavitaves).
+
+
+/*
+    descendentes de eucavitaves
+*/
+ancestral(trogoniformes, eucavitaves).
+ancestral(picocoraciae, eucavitaves).
+
+
+/*
+    descendentes de picocoraciae
+*/
+ancestral(bucerotiformes, picocoraciae).
+ancestral(coraciformes, picocoraciae).
+ancestral(piciformes, picocoraciae).
+
+
+/*
+    descendentes de australaves
+*/
+ancestral(cariamiformes, australaves).
+ancestral(eufalconimorphae, australaves).
+
+
+/*
+    descendentes de eufalconimorphae
+*/
+ancestral(falconiformes, eufalconimorphae).
+ancestral(psittacopasserae, eufalconimorphae).
+
+
+/*
+    descendentes de psittacopasserae
+*/
+ancestral(psittaciformes, psittacopasserae).
+ancestral(passeriformes, psittacopasserae).
